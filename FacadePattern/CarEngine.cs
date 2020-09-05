8e469074a8ac5c1b0b2801f0cbe137aa3b7f466f
meta-jai <52705143+meta-jai@users.noreply.github.com>
2020-09-05 23:08:39 +0530

    Initial

---
+++ FacadePattern/CarEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    /// <summary>
+    /// The 'Subsystem ClassB' class
+    /// </summary>
+    class CarEngine
+    {
+        public void SetEngine()
+        {
+            Console.WriteLine(" CarEngine - SetEngine");
+        }
+    }
+}
---
+++ FacadePattern/CarModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    /// <summary>
+    /// The 'Subsystem ClassA' class
+    /// </summary>
+    class CarModel
+    {
+        public void SetModel()
+        {
+            Console.WriteLine(" CarModel - SetModel");
+        }
+    }
+}
